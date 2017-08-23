ff8cc2cbae93c3380f14e3af300e244e73a479e8
Graciela <gmtlali88@gmail.com>
2017-08-23 14:45:38 -0700

    Implements skip, conv1x1, upsample and layers functions

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
 
 
-DEBUG = False
+DEBUG = True
 
 
 # Check TensorFlow Version
@@ -37,39 +37,50 @@ def load_vgg(sess, vgg_path):
 
     vgg_tag = 'vgg16'
     model = tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
-    
+    graph = tf.get_default_graph()
+    vgg_input_tensor_name = 'image_input:0'    
+    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_layer3_out_tensor_name = 'layer3_out:0'
+    vgg_layer4_out_tensor_name = 'layer4_out:0'
+    vgg_layer7_out_tensor_name = 'layer7_out:0'
 
-    with tf.name_scope(vgg_tag):
-	    graph = tf.get_default_graph()
+    input_tensor = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    if DEBUG:
+    	print("***********************************************************")
+    	print(input_tensor)
+    	print(keep_prob)
+    	print(layer3)
+    	print(layer4)
+    	print(layer7)
+    	print("***********************************************************")
 
 
-	    vgg_input_tensor_name = 'image_input:0'    
-	    vgg_keep_prob_tensor_name = 'keep_prob:0'
-	    vgg_layer3_out_tensor_name = 'layer3_out:0'
-	    vgg_layer4_out_tensor_name = 'layer4_out:0'
-	    vgg_layer7_out_tensor_name = 'layer7_out:0'
-
-
-
-	    input_tensor = graph.get_tensor_by_name(vgg_input_tensor_name)
-	    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
-	    layer3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
-	    layer4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
-	    layer7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
-
-
-	    if DEBUG:
-		    print("***********************************************************")
-		    print(input_tensor)
-		    print(keep_prob)
-		    print(layer3)
-		    print(layer4)
-		    print(layer7)
-		    print("***********************************************************")
-
-    return input_tensor, keep_prob, layer3, layer4, layer7
+    	return input_tensor, keep_prob, layer3, layer4, layer7
 
 tests.test_load_vgg(load_vgg, tf)
+
+
+def conv_1x1(layer, num_outputs, layer_name = "default_conv"):
+    kernel_size = 1
+
+    return tf.layers.conv2d(layer, num_outputs, kernel_size, strides = (1,1), name = layer_name)
+
+
+def upsample(layer, num_classes, layer_name = "default_upsample"):
+    """
+    """
+    kernel = 4    
+    return tf.layers.conv2d_transpose(layer, num_classes, kernel, strides = (2,2), padding = 'same', name = layer_name )
+
+
+def skip(layer_back, layer_front, layer_name = "skip"):
+	return tf.add(layer_back, layer_front, name = layer_name)
+
 
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
@@ -81,8 +92,34 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+
+    kernel_size = 1
+    stride = 1
+    layer7_1by1 =  conv_1x1(vgg_layer7_out, num_classes,"layer7_1by1")
+    layer_7_trans = upsample(layer7_1by1, num_classes, layer_name = "layer7_trans")
+
+    # 1x1 convolution of the 4th layer 
+    layer4_1by1 = conv_1x1(vgg_layer4_out, num_classes, layer_name = "layer_4_1by1")
+
+    #Add element wise the 4 layer with the output of the 7th layer
+    skip_4th = skip(layer_7_trans, layer4_1by1, layer_name = "skip_4th")
+
+    #upsample the addition of the 4th and 7th layer
+    layer_47_trans = upsample(skip_4th, num_classes, layer_name = "add47_trans")
+
+				# 1x1 convolution of the 3th layer 
+    layer3_1by1= conv_1x1(vgg_layer3_out, num_classes, "layer_3_1by1")
+
+    #Add element wise the 3 layer with the output of the last layer
+    skip_3th = skip(layer_47_trans, layer3_1by1,layer_name = "skip_3th" )
+
+				#upsample the addition of the 3th and last layer
+    output = upsample(skip_3th, num_classes, layer_name = "output")
+
+
+
+
+    return output
 tests.test_layers(layers)
 
 
@@ -96,6 +133,9 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
+
+
+
     return None, None, None
 tests.test_optimize(optimize)
 
