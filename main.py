93bd9ee26c71b46c05fb9759389e2403621ba662
Graciela <gmtlali88@gmail.com>
2017-08-24 09:40:50 -0700

    add print, increment epocst to 10 and batch size to 64

--- main.py
+++ main.py
@@ -171,6 +171,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     	for images, labels in get_batches_fn(batch_size):
     		_ , loss= sess.run([train_op, cross_entropy_loss], feed_dict = {input_image: images, correct_label:labels, keep_prob:0.5, learning_rate:0.00005})
 
+    print("Loss: {}".format(loss))
+
 tests.test_train_nn(train_nn)
 
 
@@ -189,8 +191,8 @@ def run():
     #  https://www.cityscapes-dataset.com/
 
     #HYPERPARAMETERS
-    epochs = 5
-    batch_size = 2
+    epochs = 10
+    batch_size = 64
 
     
     with tf.Session() as sess:
@@ -210,6 +212,7 @@ def run():
         nn_last_layer = layers(layer3, layer4, layer7, num_classes)
         logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
+
 
 
 
