bf7cbe9cd0605046ea01f64e7c7f5f4139192ec4
Graciela <gmtlali88@gmail.com>
2017-08-24 10:26:14 -0700

    Dropout 0.8 and learning rate 0.001

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for epoch in range(epochs):
     	print("Epoch {}...".format(epoch))
     	for images, labels in get_batches_fn(batch_size):
-    		_ , loss= sess.run([train_op, cross_entropy_loss], feed_dict = {input_image: images, correct_label:labels, keep_prob:0.5, learning_rate:0.00005})
+    		_ , loss= sess.run([train_op, cross_entropy_loss], feed_dict = {input_image: images, correct_label:labels, keep_prob:0.8, learning_rate:0.001})
 
 
     print("Loss: {}".format(loss))
